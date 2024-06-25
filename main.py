e2784c5ad43b05a42383caff4e67a84b76ac9b14
Shiv Sankhavaram <ssankha@purdue.edu>
2024-06-24 22:53:50 -0700

    Converted to sending and HTML message and working on forecast

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import smtplib
 import requests
 import os
 import json
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import datetime
+import matplotlib.pyplot as plt
 
 def fetch_credentials():
     with open("credentials.txt", "r") as file:
@@ -29,13 +33,44 @@ def get_weather_report():
     response = requests.get(url)
     
     # convert to json
-    weather_json = json.dumps(response.text)
-    print(weather_json)
-    
-    city = weather_json['name']
-    region = weather_json['region']
-    
+    weather_json = json.loads(response.text)
 
+    city = weather_json['location']['name']
+    region = weather_json['location']['region']
+    
+    # get current conditions
+    current_time, current_day = weather_json['location']['localtime'].split(' ')
+    current_tempf = weather_json['current']['temp_f']
+    current_tempc = weather_json['current']['temp_c']
+    last_updated = weather_json['current']['last_updated'].split(' ')[1] # only return the time, not the date
+    current_condition = weather_json['current']['condition']['text'].lower()
+    
+    res = "At the time that this report was delivered to you Master Wayne, it is currently " + current_condition + " at " + current_time + " and the temperature is recorded to be "  + str(current_tempf) + "\N{DEGREE SIGN} F/" + str(current_tempc) + "\N{DEGREE SIGN} C, as of " + last_updated + ").\n"
+   
+    print(res)
+    
+    # get forecast
+    day_condition = weather_json['forecast']['forecastday']['day']['condition']['text'].lower()
+    max_tempf = weather_json['forecast']['forecastday']['day']['maxtemp_f']
+    max_tempc = weather_json['forecast']['forecastday']['day']['maxtemp_c']
+    min_tempf = weather_json['forecast']['forecastday']['day']['mintemp_f']
+    min_tempc = weather_json['forecast']['forecastday']['day']['mintemp_c']
+    res += "\nThe majority of the day should be " + day_condition + ". The high will be " + str(max_tempf) + "\N{DEGREE SIGN} F/" + str(max_tempc) + " \N{DEGREE SIGN} C, and the low will be " + str(min_tempf) + "\N{DEGREE SIGN} F/" + str(min_tempc) + " \N{DEGREE SIGN} C."
+    res += "\nA graph of the forecast shown in Fahrenheit is shown below."
+    
+    # collect hourly and temperatures to generate hourly graph
+    times = []
+    temps = []
+    
+    for hourcast in weather_json['forecast']['forecastday']['hour']:
+        times.add(hourcast['time'].split(' ')[1])
+        temps.add(hourcast['temp_f'])
+        
+    # construct graph and download it 
+    
+    
+    
+    
 def main():
     weather_report = get_weather_report()
     # fetch credentials for the sender email from credentials.txt
@@ -54,18 +89,45 @@ def main():
     s.login(origin_email, origin_pass)
     
     # Construct message
+    message = MIMEMultipart("alternative")
+    message['Subject'] = str(datetime.date.today()) + " Report from Alfred"
+    message['From'] = origin_email
+    message['To'] = receive_email
+    
+    text = """\
+        Greetings, Master Wayne,"""
+
+    html = """\
+        <html>
+        <body>
+        <center>
+            <img src="https://m.media-amazon.com/images/I/31x+q3aNVKL._AC_.jpg">
+            
+            <h4>Greetings Master Wayne,\
+        """+ weather_report + """
+            </h4>
+            </center>
+        </body>
+        </html>"""
+    
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+    message.attach(part1)
+    message.attach(part2)
+    
+    # send the email
+    s.sendmail(origin_email, receive_email, message.as_string())
+    
+    # terminate the session
+    s.quit()
+    return 
     parts = ("From: " + origin_email,
          "To: " + receive_email,
          "Subject: " + "test",
          "",
-         weather_report)    
+         "Hello")    
     msg = '\r\n'.join(parts)
     
-    # send the email
-    s.sendmail(origin_email, receive_email, msg)
-    
-    # terminate the session
-    s.quit()
 
 if __name__ == "__main__":
     main()
